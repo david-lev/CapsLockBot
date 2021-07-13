413264f60042aad44ad5823b0510d55dadb87764
David Lev <davidlev050414@gmail.com>
2021-07-13 22:47:48 +0300

    Adding inline support & private mode

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import configparser
 
 config = configparser.ConfigParser()
 config.read('config.ini')
-bot_username = config.items("telegram")[0][1]
+bot_username = config.get("telegram", "bot_username")
 
 app = Client("capslock")
 
@@ -45,24 +45,22 @@ caps = {
 }
 
 
-#  replay filters
-@app.on_message(filters.reply & filters.command(["caps", f"caps@{bot_username}"]))
-def main(_, msg: types.Message):
-    gib_msg = msg.reply_to_message
-    gib = gib_msg.text.lower()
-    print(gib)
+# Convert function
+def convert(caps_str: str) -> str:
     new_str = str()
-
     #  replace capslock chars with your language:
-    for char in gib:
+    for char in caps_str.lower():
         if caps.get(char):
             new_str += caps.get(char)
-
         else:
             new_str += char
+    return new_str
 
-    gib_msg.reply(new_str)
 
+#  Group
+@app.on_message(filters.group & filters.reply & filters.command(["caps", f"caps@{bot_username}"]))
+def main(_, msg: types.Message):
+    msg.reply(convert(msg.reply_to_message.text))
     #  delete the /caps command
     try:
         msg.delete()
@@ -70,16 +68,36 @@ def main(_, msg: types.Message):
         pass
 
 
+# Private
+@app.on_message(filters.private & ~filters.command("start"))
+def private(_, msg: types.Message):
+    msg.reply(convert(msg.text))
+
+
+@app.on_inline_query()
+def inline(_, query: types.InlineQuery):
+    if not query.query:
+        return
+    query.answer([
+        types.InlineQueryResultArticle(
+            "🔠 CapsLockBot 🔠",
+            types.InputTextMessageContent(convert(query.query)),
+            description=convert(query.query),
+            thumb_url="https://telegra.ph/file/a9fe802983616d6e82db3.png")
+    ])
+
+
 #  welcome message
 @app.on_message(filters.command("start") & filters.private)
-def private(_, msg: types.Message):
+def start(_, msg: types.Message):
     txt = "Hi! Add me to your group and give me permission to delete messages; Respond to any 'capslock' message with " \
           "the command `/caps`, and I will convert it into spoken language!\n\nThis bot " \
           "made with ❤️by [David-Lev](t.me/davidlev) && [Yeuda-By](t.me/m100achuzBots)."
     msg.reply(txt, disable_web_page_preview=True,
               reply_markup=types.InlineKeyboardMarkup([
                   [types.InlineKeyboardButton("➕ Add to your group ➕",
-                                              url=f"http://t.me/{bot_username}?startgroup=true")]
+                                              url=f"http://t.me/{bot_username}?startgroup=true")],
+                  [types.InlineKeyboardButton("🔠 Converter 🔠", switch_inline_query_current_chat="RUCUYRHE")]
               ]))
 
 
